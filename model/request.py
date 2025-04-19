f260ec11051d0122cade113e1eff1e389c71084e
getinos <sujaydas0325@gmail.com>
2025-04-19 19:49:52 +0530

    scrapper and file storage

---
+++ model/request.py
@@ -0,0 +1,7 @@
+import requests
+
+url = "https://www.casekidukaan.com/rich-black-glossy-glass-case-for-iphone-xr?srsltid=AfmBOoor3Up4PC7qOoSKlUIsA1Pu-PnAI4TKO7VfOwrosofnJlBRZ2BSRzs"
+r = requests.get(url)
+
+with open("file.html", "w", encoding="utf-8") as f:
+    f.write(r.text)
