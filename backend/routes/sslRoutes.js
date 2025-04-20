e0efc4800f51ef114f9454bdb43ae8c38a47fe92
Gungun Rani <155472014+GungunRaniSahu@users.noreply.github.com>
2025-04-20 10:25:06 +0530

    Delete backend directory

--- backend/routes/sslRoutes.js
+++
@@ -1,8 +0,0 @@
-// backend/routes/sslRoutes.js
-const express = require('express');
-const router = express.Router();
-const sslController = require('../controllers/sslController');
-
-router.get('/check-ssl/:domain', sslController.checkSSL);
-
-module.exports = router;
